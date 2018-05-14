5a7d0efaab1e76f8587fc1cfdc8ace32c8c0ca73
Tosho Ivanov <todor.ait@gmail.com>
2018-05-14 09:52:30 +0300

    Updates

--- src/lib/rrInit.js
+++ src/lib/rrInit.js
@@ -2,13 +2,10 @@ import {createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {apiMiddleware} from 'redux-api-middleware'
 
-let fancyInit = (config) => {
-
-    window['fancy'] = {autoLoadAction: config.autoLoadAction}
-
+let rrInit = (config) => {
+    window['rrqd'] = {autoLoadAction: config.autoLoadAction}
     let createStoreWithMiddleware = applyMiddleware(thunkMiddleware, apiMiddleware)(createStore)
     let store = createStoreWithMiddleware(config.reducers)
-
     let routeChange = () => {
         let value = window.location.hash
         if (value && value.length > 1) {
@@ -20,24 +17,21 @@ let fancyInit = (config) => {
             store.dispatch({type: "DNM", meta: {setPublic: value}})
         }
     }
-
     // catch changes of the url after the "#" and fire an action to update the store
     window.onhashchange = routeChange
     routeChange()
-
     window.addEventListener("beforeunload", function (e) {
-        var confirmationMessage = store.getState().router.confirmRoute;
+        var confirmationMessage = store.getState().router.confirmRoute
         if (confirmationMessage) {
             //Gecko + IE
-            (e || window.event).returnValue = confirmationMessage;
+            (e || window.event).returnValue = confirmationMessage
             //Webkit, Safari, Chrome
             return "Are you sure"
         }
     })
-
     return store
 }
 
-export default fancyInit
+export default rrInit
 
 
